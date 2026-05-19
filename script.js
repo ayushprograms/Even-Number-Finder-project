714f72a286a3011e55505e77f1bcf6bab15a18fc
Ayush Programs <ayush226p@gmail.com>
2026-05-19 19:25:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var form = document.getElementById("frm");
+form.onsubmit = function () {
+	var u_input = document.getElementById("u_num").value;
+	var result = document.getElementById("result");
+	var i;
+	for (i = 1; i <= u_input; i++) {
+
+		if (i % 2 == 0) {
+			result.innerHTML += i + "<br>";
+		}
+
+	}
+	return false;
+}
